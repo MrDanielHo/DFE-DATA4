d54327d9133993178998c41af14563a9a7c603fe
triobot <daniel.kun.tin.ho@gmail.com>
2021-11-15 23:32:35 +0000

    Exercises 34-36 from Python Workbook

--- Python/IfStatements.py
+++ Python/IfStatements.py
@@ -1,9 +1,32 @@
-userScore = int(input('What did the student score?\n'))
-if userScore > 85 and userScore <= 100:
-    print("Distinction")
-elif userScore > 65 and userScore <= 85:
-    print("Pass")
-elif userScore >= 0 and userScore < 65:
-    print("Fail")
-else:
-    print("Please enter a valid score.")
+# Scoring Exercise
+# userScore = int(input('What did the student score?\n'))
+# if userScore > 85 and userScore <= 100:
+#     print("Distinction")
+# elif userScore > 65 and userScore <= 85:
+#     print("Pass")
+# elif userScore >= 0 and userScore < 65:
+#     print("Fail")
+# else:
+#     print("Please enter a valid score.")
+
+# Exercise 34: Even or Odd?
+# intCheck = int(input("Please enter a number:\n"))
+# if intCheck % 2 == 0:
+#     print("Even number")
+# else:
+#     print("Odd number")
+
+# Exercise 35: Dog Years
+# humanYears = float(input("What is your age?\n"))
+# if humanYears >= 1:
+#     print("You are " + str( int( 10.5 + (humanYears - 2) * 4 ) ) + " in dog years.")
+# else:
+#     print("You are a dog.")
+
+# Exercise 36:Vowel or Consonant
+# letterCheck = str(input('Please enter a letter:\n'))
+# lowerCheck = letterCheck.lower()
+# if lowerCheck in "aeiou":
+#     print("Vowel")
+# else:
+#     print("Consonant")
