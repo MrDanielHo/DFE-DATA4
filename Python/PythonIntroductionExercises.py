7426d4fe76eaa4eb5720a6e715959ec0cce1c602
triobot <daniel.kun.tin.ho@gmail.com>
2021-11-15 11:23:02 +0000

    First set of exercises.

---
+++ Python/PythonIntroductionExercises.py
@@ -0,0 +1,16 @@
+# Errors
+# PRINT "Hello World" - needs to be lowercase print and needs parenthesis around the inverted commas
+# print(Hello World) - needs inverted commas inside the parenthesis
+# Print("Hello World") - capital P needs to be lowercase
+# priint("Hello World") - needs to be print, not priint
+
+print( "What is your first name?")
+firstname = input()
+print ( "What is your family name?")
+familyname = input()
+print ("Hello " + firstname + " " + familyname + "!")
+number1 = float( input ( "Please enter the first number: " ))
+number2 = float( input ( "Please enter the second number: " ))
+answer = number1 + number2
+
+print(number1, "+", number2, "=", answer)
