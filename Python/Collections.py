2a13f8c601e9dfa71a94fd65dbb08b7b83c1565e
triobot <daniel.kun.tin.ho@gmail.com>
2021-11-15 15:10:38 +0000

    Added Collections exercises.

---
+++ Python/Collections.py
@@ -0,0 +1,10 @@
+# shoppingList = ["Meat", "Vegetable", "Cake", "Beverages for the weekend."]
+# print(shoppingList[)
+
+# cool_cows = ["Winnie the Moo", "Moolan", "Milkshake", "Mooana"]
+# cool_sheep = ["Baaaart", "Baaaarnaby"]
+# cool_pigs = ["Chris P. Bacon", "Hamlet", "Hogwarts"]
+# cool_animals = [cool_cows, cool_sheep, cool_pigs]
+
+books = {"The Handmaiden's Tale":"Margaret Atwood", "The Hobbit":"J.R.R. Tolkien", "Charlie and the Chocolate Factory":"Roald Dahl"}
+print(books.values())
