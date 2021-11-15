4a12f66820ad2917ee6d7908eabfb36c58869157
triobot <daniel.kun.tin.ho@gmail.com>
2021-11-15 23:10:52 +0000

    Added upper/lower boundry limits of grades.

--- Python/IfStatements.py
+++ Python/IfStatements.py
@@ -1,10 +1,9 @@
-userScore = int(input('What did the student score? '))
-if type(userScore) == int:
-    if int(userScore) > 85:
-        print("Distinction")
-    elif int(userScore) > 65:
-        print("Pass")
-    else:
-        print("Fail")
+userScore = int(input('What did the student score?\n'))
+if userScore > 85 and userScore <= 100:
+    print("Distinction")
+elif userScore > 65 and userScore <= 85:
+    print("Pass")
+elif userScore >= 0 and userScore < 65:
+    print("Fail")
 else:
     print("Please enter a valid score.")
