d9822e77997cb3ddb3eec02caa10568a9d37df02
triobot <daniel.kun.tin.ho@gmail.com>
2021-11-16 20:05:54 +0000

    w3 function exercises 1 and 2

--- Python/Functions.py
+++ Python/Functions.py
@@ -1,7 +1,7 @@
-# Argos
-# from typing import final
-
-
+'''
+Argos
+From typing import final
+'''
 # def stock_stuff(input_variable1,input_variable2):
 #     product_code_length = len(input_variable1)
 #     if int(input_variable2)%2 == 0:
@@ -30,29 +30,60 @@ Reminder: inputs and prints should not be included inside the function definitio
 Stretch goal: Include grade boundaries such that the program also outputs a grade for the student (A, B, etc.)
 '''
 # Final Grade
-def calcFinalScore(int_homeworkScore, int_assessmentScore, int_examScore):
-    finalScore = (int(int_homeworkScore) + int(int_assessmentScore) + int(int_examScore)) / 175 * 100
-    if int(finalScore) >= 95:
-        gradedScore = "A*"
-    elif int(finalScore) >= 85:
-        gradedScore = "A"
-    elif int(finalScore) >= 75:
-        gradedScore = "B"
-    elif int(finalScore) >= 65:
-        gradedScore = "C"
-    elif int(finalScore) >= 55:
-        gradedScore = "D"
-    elif int(finalScore) >= 45:
-        gradedScore = "E"
-    else:
-        gradedScore = "F"
-    return (finalScore, gradedScore)
+# def calcFinalScore(int_homeworkScore, int_assessmentScore, int_examScore):
+#     finalScore = (int(int_homeworkScore) + int(int_assessmentScore) + int(int_examScore)) / 175 * 100
+#     if int(finalScore) >= 95:
+#         gradedScore = "A*"
+#     elif int(finalScore) >= 85:
+#         gradedScore = "A"
+#     elif int(finalScore) >= 75:
+#         gradedScore = "B"
+#     elif int(finalScore) >= 65:
+#         gradedScore = "C"
+#     elif int(finalScore) >= 55:
+#         gradedScore = "D"
+#     elif int(finalScore) >= 45:
+#         gradedScore = "E"
+#     else:
+#         gradedScore = "F"
+#     return (finalScore, gradedScore)
 
-studentName = input('Student Name:\n')
-homeworkScore = input('What did ' + studentName + ' score on their homework?\n')
-assessmentScore = input('What did ' + studentName + ' score on their assessment?\n')
-examScore = input('What did ' + studentName + ' score on their final exam?\n')
+# studentName = input('Student Name:\n')
+# homeworkScore = input('What did ' + studentName + ' score on their homework?\n')
+# assessmentScore = input('What did ' + studentName + ' score on their assessment?\n')
+# examScore = input('What did ' + studentName + ' score on their final exam?\n')
 
-totalScore = calcFinalScore(homeworkScore, assessmentScore, examScore)
-# print(totalScore)
-print(studentName + " got a " + totalScore[1] + " (" + str(round(totalScore[0],2)) + ").")
+# totalScore = calcFinalScore(homeworkScore, assessmentScore, examScore)
+# # print(totalScore)
+# print(studentName + " got a " + totalScore[1] + " (" + str(round(totalScore[0],2)) + ").")
+
+'''
+https://www.w3resource.com/python-exercises/python-functions-exercises.php
+01. Write a Python function to find the Max of three numbers. 
+'''
+# def maximum(x, y, z):
+#     if ( x > y ) and ( x > z ):
+#         largest = x
+#     elif ( y > x ) and ( y > z ):
+#         largest = y
+#     else:
+#         largest = z
+#     return largest
+# x = input("1st Number: ")
+# y = input("2nd Number: ")
+# z = input("3rd Number: ")
+# biggest = (maximum(x,y,z))
+# print(biggest)
+
+'''
+02. Write a Python function to sum all the numbers in a list.
+Sample List : (8, 2, 3, 0, 7)
+Expected Output : 20 
+'''
+def sumOfNum(*listOfIntegers):
+    total = 0
+    for x in listOfIntegers:
+        total += x
+    return total
+
+print(sumOfNum(8, 2, 3, 0, 7))
