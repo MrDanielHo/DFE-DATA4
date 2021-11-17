27e5bbdd50a37adf41a3cf4af32a274b3c896138
triobot <daniel.kun.tin.ho@gmail.com>
2021-11-17 11:06:55 +0000

    Created modules to be used in other programs.

---
+++ Python/LearningModules/Dungeons&Dragon.py
@@ -0,0 +1,2 @@
+from dice import d6
+print('1st roll: ' + str(d6()) + '\n2nd roll: ' + str(d6()))
---
+++ Python/LearningModules/dice.py
@@ -0,0 +1,5 @@
+import random
+def d6():
+    return random.randint(1,6)
+def d20():
+    return random.randint(1,20)
